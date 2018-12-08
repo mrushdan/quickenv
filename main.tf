e7658921ce15da2e222d7a4e2218201a34c7c687
mrushdan <mrushdan@gmail.com>
2018-12-08 10:03:56 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,65 @@
+#--------Default/main.tf
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+#---------------------------------------------Modules
+
+#--------------------------------Storage
+
+module "storage" {
+  source       = "./storage"
+  project_name = "${var.project_name}"
+}
+
+
+#--------------------------------Networking
+
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip     = "${var.accessip}"
+}
+
+
+#---------------------------------Compute
+
+module "compute" {
+  source          = "./compute"
+  instance_count  = "${var.instance_count}"
+  key_name        = "${var.key_name}"
+  public_key_path = "${var.public_key_path}"
+  instance_type   = "${var.server_instance_type}"
+  subnets         = "${module.networking.public_subnets}"
+  security_group  = "${module.networking.public_sg}"
+  subnet_ips      = "${module.networking.subnet_ips}"
+}
+
+
+
+#--------------------------------------------------------------IAM
+
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name = "s3_access"
+  role = "${aws_iam_role.s3_access_role.name}"
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
